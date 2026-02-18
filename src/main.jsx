fc95b2bb25350d50a4687c4840d7e222977b1d09
DPGorman <157316753+DPGorman@users.noreply.github.com>
2026-02-19 02:38:37 +0700

    2nd commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SignalApp from "./signal-app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SignalApp />
+  </React.StrictMode>
+);
