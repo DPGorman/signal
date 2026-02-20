2ecfb761ebed4b3b0760118ee81770c5ac8f4061
DPGorman <157316753+DPGorman@users.noreply.github.com>
2026-02-20 08:12:52 +0700

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+ import { useState, useEffect, useRef } from "react";
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://krhidwibweznwakaoxjw.supabase.co";
