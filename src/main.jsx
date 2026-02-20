e0d865037a0176c96401b3911a154f4eb9920ba4
DPGorman <157316753+DPGorman@users.noreply.github.com>
2026-02-20 08:55:15 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SignalDashboard from "./signal-app.jsx";
-
+import SignalDashboard from "./app.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SignalDashboard />
